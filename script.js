d5cc696849ee7aaacd407eda845b7bb36209a9fb
odhiambovinogola <141261098+odhiambovinogola@users.noreply.github.com>
2025-10-31 14:37:56 +0300

    feat(script): generate random computer choice
    Implement getComputerChoice() to return a random value: "rock", "paper", or "scissors".

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+// step 1: Logic to get the computer choice
+// - create a function getComputerChoice
+// - randomly returns “rock”, “paper” or “scissors”
+
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  let random = choices[Math.floor(Math.random() * choices.length)];
+  return random;
+}
