95fc1b942c4b5e543a1aa5c038d22c6eb80a372c
odhiambovinogola <141261098+odhiambovinogola@users.noreply.github.com>
2025-11-01 07:40:00 +0300

    feat(script): add getHumanChoice to prompt user for choice
    Assume user input is valid. Input validation and re-prompting will be added later.

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function getComputerChoice() {
   let random = choices[Math.floor(Math.random() * choices.length)];
   return random;
 }
+
+// Step 2: logic to get the human choice
+// - function name: getHumanChoice
+// - get user input
+// - return user input
+
+function getHumanChoice() {
+  let choice = prompt("What is your pick?", "rock");
+  return choice;
+}
